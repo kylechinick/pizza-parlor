8f757c95b6fdeb741450be550b7fdea3821a0a7b
Kyle Chinick <kylechinick@icloud.com>
2022-06-17 09:15:27 -0700

    Add initial notes for project planning: mvp requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log('Connection test');
+
+// ROAD MAP
+// Primary function of app: user can build a pizza and see total cost
+
+// 1. User can add 1 or more toppings to pizza
+// 2. User must choose pizza size
+
+// Technical MVP Requirements:
+// - Make/use a constructor
+// - Make/use a prototype
+// - Include tests for all business logic (commit after each successful test)
+// - Complete README
